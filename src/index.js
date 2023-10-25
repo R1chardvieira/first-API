8fb5d560ab28c9aba981cf60cecf7905a9d1b13a
R1chardvieira <luckasrichard22@gmail.com>
2023-10-24 22:06:16 -0300

    endpoint responsavel por listar os usuarios da application

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 const http = require('http');
+const users = require('./mocks/users');
 
 const server = http.createServer((request,response)=>{
-    response.writeHead(200,{'content-type':'text/html'});
-    response.end('<h1>SERVER TEST<h1>');
+    console.log(`Request method: ${request.method} | Endpoint: ${request.url}`);        //retorna o metodo ex:GET e o endpoint
+
+    if(request.url === '/users' && request.method === 'GET'){
+        response.writeHead(200, { 'content-type':'application/json'} );
+        response.end(JSON.stringify(users)); //usa a function json.stringfy para transformar array/objetos em string
+    }else{
+        response.writeHead(404,{'content-type':'text/html'});
+        response.end(`Cannot ${request.method} ${request.url}`);
+    }
+
+
+
 });
 ''
 server.listen(3000, ()=>console.log('🔥 Server started at http://localhost:3000'));
---
+++ src/mocks/users.js
@@ -0,0 +1,14 @@
+module.exports=[
+    {
+        id:1,
+        name:'Richard',
+    },
+    {
+        id:2,
+        name:'Lorena',
+    },
+    {
+        id:3,
+        name:'Ricardo',
+    }
+];
